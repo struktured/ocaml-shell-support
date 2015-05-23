bf049939b3e976e1d7074a2d015436901814b9c8
cpiccion <carmelo.piccione@theice.com>
2015-05-22 23:38:02 -0400

    YJ-7511 decompress fixes

--- lib/decompress.ml
+++ lib/decompress.ml
@@ -1,22 +1,30 @@
+
 module Extension = 
-struct type t = [`gz | `bz2 | `zip] [@@deriving show] 
+struct type t = [`gz | `bz2 | `zip| `tgz]
 
 let of_string s = String.lowercase s |> function 
-  | str when show `gz = str -> `Ok `gz
-  | str when show `bz2 = str -> `Ok `bz2
-  | str when show `zip = str -> `Ok `zip
+  | "gz" -> `Ok `gz
+  | "bz2" -> `Ok `bz2
+  | "zip" -> `Ok `zip
+  | "tgz" -> `Ok `tgz
   | str -> `Error (false, "unsupported extension: " ^ str)
 
-let to_string = show
+let to_string = function
+  | `gz -> "gz"
+  | `bz2 -> "bz2"
+  | `zip -> "zip"
+  | `tgz -> "tgz"
 end
 
 let print s = print_endline @@ Printf.sprintf "[decompress] %s" s
 
-let cmd_for filename = let open Shell_utils.Infix in 
-  Extension.of_string filename >>| function
+let cmd_for filename = let open Shell_utils.Infix in
+  FilePath.get_extension filename |>
+  Extension.of_string >>| function
   | `gz -> "gzip -f -d " ^ filename
   | `bz2 -> "bzip2 -f -d " ^ filename
   | `zip -> "unzip -o " ^ filename
+  | `tgz -> "tar zxvf " ^ filename
 
 let _clean_maybe ~clean filename =
   if not clean then `Ok ("skipped clean for: " ^ filename) else
@@ -26,8 +34,8 @@ let _clean_maybe ~clean filename =
         FileUtil.rm 
           ~recurse:true 
           ~force:FileUtil.Force 
-          [chopped; FilePath.chop_extension chopped]; 
-        `Ok filename
+          [chopped];
+        `Ok chopped
       with e -> `Error (true, "failed to clean uncompressed targets of " ^ filename)
     end
 
